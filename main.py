68367ff0c9a8ac7d232afb35dc246f339af72d0b
willynajberg <guille19981@gmail.com>
2020-11-13 14:26:51 +0100

    Codigo comentado y arreglo de pequeños fallos relacionados con la busqueda de Google y el boton de refrescar

--- main.py
+++ main.py
@@ -15,8 +15,10 @@ class Main(QMainWindow):
         super(Main, self).__init__()
         var.ui = Ui_MainWindow()
         var.ui.setupUi(self)
+
         self.nueva_pestana(URL_HOME)
 
+        # conexión de funciones con eventos del navegador:
         var.ui.editUrl.returnPressed.connect(self.navegar_a_url)
         var.ui.tabWidget.currentChanged.connect(self.pestana_cambiada)
         var.ui.tabWidget.tabCloseRequested.connect(self.cerrar_pestana)
@@ -25,66 +27,94 @@ class Main(QMainWindow):
         var.ui.btnAtras.clicked.connect(lambda: var.ui.tabWidget.currentWidget().back())
         var.ui.btnAdelante.clicked.connect(lambda: var.ui.tabWidget.currentWidget().forward())
 
+        # al principio deshabilita los botones de atras y de delante
         var.ui.btnAtras.setDisabled(True)
         var.ui.btnAdelante.setDisabled(True)
 
     def nueva_pestana(self, url):
         try:
+            # antes de nada cambia el botón de refrescar por el boton de cancelar carga
+            self.cambiar_btnrefrescar(True)
+
+            # crea un objeto QWebEngineView nuevo y lo asignamos a una pestaña nueva del TableWidget
             navegador = QWebEngineView()
             self.setWindowTitle("PyQtBrowser - Pestaña nueva")
             i = var.ui.tabWidget.addTab(navegador, "Pestaña nueva")
             var.ui.tabWidget.setCurrentIndex(i)
 
+            # cargamos la url nueva en el nuevo objeto y en la barra de urls
             navegador.setUrl(QUrl(url))
             var.ui.editUrl.setText(url)
 
+            # conexion de eventos del objeto QWebEngineView con funciones del navegadorÑ
+
             navegador.urlChanged.connect(lambda qurl, nav=navegador:
                                          self.actualizar_url(qurl, nav))
-
             navegador.iconChanged.connect(lambda icon, nav=navegador:
                                           self.actualizar_icono(icon, nav))
 
-            navegador.loadStarted.connect(lambda: self.cambiar_btnrefrescar(True))
+            # esto seria lo optimo, pero no funciona bien, por algun motivo hay paginas como el buscador de google
+            # que llaman a este evento 2 veces:
+            # navegador.loadStarted.connect(lambda: self.cambiar_btnrefrescar(True))
 
+            navegador.loadFinished.connect(self.actualizacion_completada)
             navegador.loadFinished.connect(lambda _, nav=navegador:
                                            self.actualizar_titulo(nav))
 
-            navegador.loadFinished.connect(self.actualizacion_completada)
-
+            # para que la pestaña de añadir pestañas aparezca al final, la borra y la vuelve a añadir
             var.ui.tabWidget.removeTab(var.ui.tabWidget.indexOf(var.ui.tabAnadir))
             i2 = var.ui.tabWidget.addTab(var.ui.tabAnadir, "+")
 
+            # esta funcion oculta el boton de cerrar de la pestaña de añadir
             var.ui.tabWidget.tabBar().tabButton(i2, var.ui.tabWidget.tabBar().RightSide).resize(0, 0)
         except Exception as error:
             print("Error: %s" % str(error))
 
     def actualizar_titulo(self, navegador):
+        # Funcion conectada al evento loadFinished de un objeto QWebEngineView. Cuando una página acabe de cargar,
+        # cambiará el título de la pestaña asignada a ese QWebEngineView al título de la página web
         try:
             titulo = navegador.page().title()
-            fm = var.ui.tabWidget.fontMetrics()
 
+            # Este código se asegurará de que los títulos de páginas largos se acortarán
+            fm = var.ui.tabWidget.fontMetrics()
             i = len(titulo)
             while fm.width(titulo) > 185:
                 titulo = titulo[:i] + "..."
                 i = i - 1
 
+            # Si la pestaña que tenemos abierta es la del QWebEngineView que ha activado el evento, cambia el título
+            # de la ventana también
             if navegador == var.ui.tabWidget.currentWidget():
                 self.setWindowTitle("PyQtBrowser - %s" % titulo)
 
+            # Cambia el título de la pestaña del QWebEngineView que ha llamado al evento
             var.ui.tabWidget.setTabText(var.ui.tabWidget.indexOf(navegador), titulo)
         except Exception as error:
             print("Error: %s" % str(error))
 
     def actualizar_icono(self, icono, navegador):
+        # Esta función funciona igual que la funcion de actualizar_titulo, solo que en este caso es llamada por el
+        # evento iconChanged de un QWebEngine view, y le asignamos el icono de la página a la pestaña
         try:
-            var.ui.tabWidget.setTabIcon(var.ui.tabWidget.indexOf(navegador), icono)
+            if icono:
+                var.ui.tabWidget.setTabIcon(var.ui.tabWidget.indexOf(navegador), icono)
+            else:
+                # si han pasado un icono nulo, pasa un icono en blanco
+                var.ui.tabWidget.setTabIcon(var.ui.tabWidget.indexOf(navegador), QIcon())
         except Exception as error:
             print("Error: %s" % str(error))
 
     def actualizar_url(self, url, navegador):
+        # Esta funcion es llamada por el evento urlChanged de un QWebEngineView. Cuando este evento se ejecute
+        # cambiara la barra de url del navegador, el icono de la pestana, habilitará los botones de atras y adelante
+        # si es posible y cambiara el boton de refrescar por el de cancelar
         try:
             if navegador == var.ui.tabWidget.currentWidget():
+                self.cambiar_btnrefrescar(True)
+                self.actualizar_icono(None, navegador)
                 var.ui.editUrl.setText(url.toString())
+                # Asegura que veremos el principio de la url en el LineEdit
                 var.ui.editUrl.setCursorPosition(0)
 
                 var.ui.btnAtras.setEnabled(navegador.history().canGoBack())
@@ -93,6 +123,8 @@ class Main(QMainWindow):
             print("Error: %s" % str(error))
 
     def cambiar_btnrefrescar(self, cargando):
+        # Esta funcion es la encargada de cambiar el botón de refrescar por el de cancelar carga, segun el booleano
+        # que se le pase en los parametros
         try:
             if cargando:
                 icon = QtGui.QIcon()
@@ -110,18 +142,25 @@ class Main(QMainWindow):
             print("Error : %s" % str(error))
 
     def refrescar(self):
+        # Funcion conectada al evento clicked del boton de refrescar. Refresca la página actual y cambia el boton de
+        # refrescar por el de cancelar carga
         try:
+            self.cambiar_btnrefrescar(True)
             var.ui.tabWidget.currentWidget().reload()
         except Exception as error:
             print("Error: %s" % str(error))
 
     def actualizacion_completada(self):
+        # Funcion conectada al evento loadFinished de un QWebEngineView que simplemente cambiará el boton de cancelar
+        # carga por el de refrescar de nuevo
         try:
             self.cambiar_btnrefrescar(False)
         except Exception as error:
             print("Error: %s" % str(error))
 
     def cancelar_actualizacion(self):
+        # Funcion conectada al evento clicked del boton de cancelar carga que parará la carga de la pestaña actual
+        # y cambiara el boton de cancelar carga por el de refrescar nuevamente
         try:
             var.ui.tabWidget.currentWidget().stop()
             self.actualizacion_completada()
@@ -129,28 +168,47 @@ class Main(QMainWindow):
             print("Error: %s" % str(error))
 
     def navegar_a_url(self):
+        # Esta funcion esta conectada al evento returnPressed del lineEdit de la barra de URL. Es decir, se activara
+        # cuando el usuario pulse Enter en la barra de url.
         try:
+            # Primero cambia el boton de refrescar al de cancelar carga.
+            self.cambiar_btnrefrescar(True)
+            # Coge el texto del line edit
             url = var.ui.editUrl.text()
-            es_enlace = re.search("[a-z][.][a-z]", url)
+
+            # Usa un patron de RegEx para comprobar si lo que el usuario ha introducido es un enlace o una IP
+            es_enlace = re.search("(([a-zA-Z]|[0-9])[.]([a-zA-Z]|[0-9]))|[a-z][/]$", url)
+
             if es_enlace:
+                # Si es un enlace, creara un objeto QUrl con el enlace introducido
                 qurl = QUrl(url)
+
+                # Si el usuario no ha puesto http:// o https:// al principio, pondra el protocolo http automaticamente
                 if qurl.scheme() == "":
                     qurl.setScheme("http")
             else:
+                # Si lo que el usuario ha puesto no es un enlace, reemplaza los espacios en blanco por + y utiliza
+                # el enlace de busqueda establecido para buscar lo introducido en internet.
                 qurl = QUrl(URL_BUSQUEDA % url.replace(" ", "+"))
 
+            # Finalmente cambia a la QUrl generada.
             var.ui.tabWidget.currentWidget().setUrl(qurl)
         except Exception as error:
             print("Error: %s" % str(error))
 
     def navegar_a_home(self):
+        # Funcion conectada al boton home del navegador que simplemente navegara al enlace establecido como enlace home
         try:
+            self.cambiar_btnrefrescar(True)
             var.ui.tabWidget.currentWidget().setUrl(QUrl(URL_HOME))
         except Exception as error:
             print("Error: %s" % str(error))
 
     def pestana_cambiada(self, i):
+        # Funcion conectada al evento tabChanged del TabWidget del navegador que se encarga de cambiar el titulo de la
+        # ventana principal, la url del line edit, el boton de refrescar y los botones de atras y adelante.
         try:
+            # Si la pestaña a la que el usuario se quiere desplazar es la pestaña de añadir, añade una nueva
             if i == var.ui.tabWidget.indexOf(var.ui.tabAnadir):
                 self.nueva_pestana(URL_HOME)
             else:
@@ -165,13 +223,20 @@ class Main(QMainWindow):
             print("Error: %s" % str(error))
 
     def cerrar_pestana(self, i):
+        # Funcion conectada al evento tabCloseRequested del TabWidget del navegador que se activará cuando el usuario
+        # cierre una de las pestañas
         try:
+            # Si la pestaña a cerrar es la de añadir pestañas no hace nada
             if i == var.ui.tabWidget.indexOf(var.ui.tabAnadir):
                 return
             else:
+                # Si la pestaña a cerrar esta justo antes de la de añadir, cambia la vista a una pestaña anterior, si es
+                # que la hay
                 if var.ui.tabWidget.count() > 2:
                     if i + 1 == var.ui.tabWidget.indexOf(var.ui.tabAnadir):
                         var.ui.tabWidget.setCurrentIndex(i - 1)
+
+                # Finalmente borra la pestaña
                 var.ui.tabWidget.removeTab(i)
         except Exception as error:
             print("Error: %s" % str(error))
