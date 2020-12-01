53fff2abfa0c2fa38c770da7e7a5fc5bc629e047
willynajberg <wilhelm.najberg@wp.pl>
2020-12-01 14:26:13 +0100

    0.2.0 - Arreglado fallo cuando javascript solicita una ventana nueva. Codigo comentado

---
+++ hilo_trabajador.py
@@ -0,0 +1,75 @@
+from PyQt5 import QtCore
+from PyQt5.QtCore import QThread
+
+import conexion
+import var
+
+
+# Esta es una clase auxiliar para crear un hilo que se encargue del CRUD
+# Este se ejecutará en un hilo separado del hilo principal para optimizar el rendimiento
+class HiloTrabajador(QThread):
+    historialRecibido = QtCore.pyqtSignal(object)
+    favoritosRecibidos = QtCore.pyqtSignal(object)
+    paginaFavorita = QtCore.pyqtSignal(bool)
+
+    def __init__(self):
+        super(HiloTrabajador, self).__init__()
+        self.running = False
+        self.tareas = []
+
+    def run(self):
+        conexion.conectardb(var.NOMBRE_BD)
+        self.running = True
+
+        while self.running:
+            for tarea in self.tareas:
+                tarea()
+                self.tareas.remove(tarea)
+
+            # Ahorra recursos reduciendo la frecuencia con la que se ejecuta el hilo
+            self.msleep(20)
+
+    def anadir_tarea(self, tarea):
+        self.tareas.append(tarea)
+
+        if not self.running:
+            self.start()
+
+    def anadir_historial(self, navegador):
+        no_insertar = (conexion.seleccionar_ultima_url() and str(conexion.seleccionar_ultima_url()) == str(
+            navegador.url().toString())) or navegador.url().toString() == "about:blank"
+
+        if not no_insertar:
+            conexion.insertar_historial(navegador.url().toString(), navegador.page().title())
+        else:
+            conexion.cambiar_titulo_historial(var.LAST_INSERT_HISTORIAL, navegador.page().title())
+
+    def cargar_historial(self):
+        query = conexion.cargar_historial()
+
+        try:
+            self.historialRecibido.emit(query)
+        except Exception as error:
+            print("Error: %s" % str(error))
+
+    def cargar_favoritos(self):
+        query = conexion.cargar_favoritos()
+
+        try:
+            self.favoritosRecibidos.emit(query)
+        except Exception as error:
+            print("Error: %s" % str(error))
+
+    def comprobar_favorito(self, url):
+        es_fav = conexion.comprobar_favorito(url)
+
+        self.paginaFavorita.emit(es_fav)
+
+    def borrar_entrada(self, idx):
+        conexion.borrar_entrada_historial(idx)
+
+    def parar(self):
+        while len(self.tareas) > 0:
+            continue
+
+        self.running = False
--- main.py
+++ main.py
@@ -9,85 +9,17 @@ from PyQt5.QtWebEngineWidgets import *
 from PyQt5.QtWidgets import *
 
 from WidgetHistorial import WidgetHistorial
+from hilo_trabajador import HiloTrabajador
 from ventana import *
 
 
 class Main(QMainWindow):
-    class HiloHistorial(QThread):
-        historialRecibido = QtCore.pyqtSignal(object)
-        favoritosRecibidos = QtCore.pyqtSignal(object)
-        paginaFavorita = QtCore.pyqtSignal(bool)
-
-        def __init__(self):
-            super(Main.HiloHistorial, self).__init__()
-            self.running = False
-            self.tareas = []
-
-        def run(self):
-            conexion.conectardb(var.NOMBRE_BD)
-            self.running = True
-
-            while self.running:
-                for tarea in self.tareas:
-                    tarea()
-                    self.tareas.remove(tarea)
-
-                # Ahorra recursos reduciendo la frecuencia con la que se ejecuta el hilo
-                self.msleep(20)
-
-        def anadir_tarea(self, tarea):
-            self.tareas.append(tarea)
-
-            if not self.running:
-                self.start()
-
-        def anadir_historial(self, navegador):
-            no_insertar = conexion.seleccionar_ultima_url() and str(conexion.seleccionar_ultima_url()) == str(
-                navegador.url().toString())
-
-            if not no_insertar:
-                conexion.insertar_historial(navegador.url().toString(), navegador.page().title())
-            else:
-                conexion.cambiar_titulo_historial(var.LAST_INSERT_HISTORIAL, navegador.page().title())
-
-        def cargar_historial(self):
-            query = conexion.cargar_historial()
-
-            try:
-                self.historialRecibido.emit(query)
-            except Exception as error:
-                print("Error: %s" % str(error))
-
-        def cargar_favoritos(self):
-            query = conexion.cargar_favoritos()
-
-            try:
-                self.favoritosRecibidos.emit(query)
-            except Exception as error:
-                print("Error: %s" % str(error))
-
-        def comprobar_favorito(self, url):
-            es_fav = conexion.comprobar_favorito(url)
-
-            self.paginaFavorita.emit(es_fav)
-
-        def borrar_entrada(self, idx):
-            conexion.borrar_entrada_historial(idx)
-
-        def parar(self):
-            while len(self.tareas) > 0:
-                continue
-
-            self.running = False
-
     def __init__(self):
         super(Main, self).__init__()
         var.ui = Ui_MainWindow()
         var.ui.setupUi(self)
 
-        var.carga_finalizada = False
-        var.progreso_actual = 0
-
+        # Menu de la esquina de la derecha:
         var.menu = QMenu(var.ui.btnMenu)
         var.menu.addAction(var.ui.actionNewTab)
         var.menu.addAction(var.ui.actionCloseTab)
@@ -121,10 +53,12 @@ class Main(QMainWindow):
         var.ui.btnAtras.setDisabled(True)
         var.ui.btnAdelante.setDisabled(True)
 
-        self.hilo_historial = Main.HiloHistorial()
+        # Crea una nueva instancia del hilo trabajador
+        self.hilo_trab = HiloTrabajador()
 
-        self.hilo_historial.favoritosRecibidos.connect(self.mostrar_favoritos)
-        self.cargar_favoritos()
+        # Conecta la señal emitida por el hilo al recibir las paginas favoritas de la BD con la funcion del navegador
+        self.hilo_trab.favoritosRecibidos.connect(self.mostrar_favoritos)
+        self.hilo_trab.anadir_tarea(self.hilo_trab.cargar_favoritos)
 
         self.nueva_pestana(var.URL_HOME)
 
@@ -150,23 +84,46 @@ class Main(QMainWindow):
 
             # esta funcion oculta el boton de cerrar de la pestaña de añadir
             var.ui.tabWidget.tabBar().tabButton(i2, var.ui.tabWidget.tabBar().RightSide).resize(0, 0)
+
+            return navegador
         except Exception as error:
             print("Error: %s" % str(error))
+            return None
 
     def conectar_nav(self, navegador):
         try:
-            navegador.page().urlChanged.connect(lambda qurl, nav=navegador:
+            navegador.urlChanged.connect(lambda qurl, nav=navegador:
                                                 self.cambiar_url(qurl, nav))
-            navegador.page().iconChanged.connect(lambda icon, nav=navegador:
+            navegador.iconChanged.connect(lambda icon, nav=navegador:
                                                  self.actualizar_icono(icon, nav))
 
-            navegador.page().loadStarted.connect(lambda nav=navegador: self.carga_iniciada(nav))
-            navegador.page().loadProgress.connect(lambda progreso, nav=navegador: self.progreso_carga(progreso, nav))
-            navegador.page().loadFinished.connect(lambda _, nav=navegador: self.carga_completada(nav))
+            navegador.loadStarted.connect(lambda nav=navegador: self.carga_iniciada(nav))
+            navegador.loadProgress.connect(lambda progreso, nav=navegador: self.progreso_carga(progreso, nav))
+            navegador.loadFinished.connect(lambda _, nav=navegador: self.carga_completada(nav))
+
+            # Sobreescribe la funcion de createWindow del QWebEngineView con la definida abajo
+            # Esta funcion es llamada por elementos javascript de las paginas que abren ventanas nuevas
+            navegador.createWindow = self.createWindow
+        except Exception as error:
+            print("Error: %s" % str(error))
+
+    def createWindow(self, type):
+        # Al intentar crear una pestaña nueva con javascript, este espera que el navegador en el que se ejecuta le retorne
+        # un puntero a un objeto de tipo QWebEnginePage en el que pueda escribir una url nueva
+        try:
+            nueva = self.nueva_pestana("")
+            nuevapag = QWebEnginePage()
+            nueva.setAttribute(Qt.WA_DeleteOnClose, True)
+            nueva.setPage(nuevapag)
+            nueva.show()
+
+            return nueva
         except Exception as error:
             print("Error: %s" % str(error))
 
     def carga_iniciada(self, navegador):
+        # Al empezar a cargar una página, comprobará si esta página está en los favoritos, y cambiará el boton de refrescar
+        # por el de cancelar carga.
         try:
             self.comprobar_fav(navegador.page().url().toString())
 
@@ -188,8 +145,7 @@ class Main(QMainWindow):
         # al de refrescar, de actualizar el título y de insertar una nueva entrada en el historial, o en caso de que
         # la última entrada del historial sea la misma url, actualizar su título
         try:
-            self.hilo_historial.anadir_tarea(lambda nav=navegador: self.hilo_historial.anadir_historial(nav))
-
+            self.hilo_trab.anadir_tarea(lambda nav=navegador: self.hilo_trab.anadir_historial(nav))
             self.actualizacion_completada()
             self.actualizar_icono(navegador.page().icon(), navegador)
             self.actualizar_titulo(navegador)
@@ -258,6 +214,9 @@ class Main(QMainWindow):
             print("Error: %s" % str(error))
 
     def actualizar_icono_fav(self, es_fav=False):
+        # Esta funcion es la encargada de cambiar la apariencia del botón de favoritos si la página en la que estamos
+        # Está marcada como favorita, y desconectará las funciones conectadas a su evento clicked y las reemplazará
+        # por las indicadas
         try:
             if es_fav:
                 icon = QtGui.QIcon()
@@ -366,6 +325,7 @@ class Main(QMainWindow):
 
                     var.ui.btnAtras.setEnabled(widget_actual.history().canGoBack())
                     var.ui.btnAdelante.setEnabled(widget_actual.history().canGoForward())
+                # Si el widget actual es la pestaña de favoritos, para evitar errores deshabilitará los botones de navegacion
                 elif isinstance(widget_actual, WidgetHistorial):
                     var.ui.btnAtras.setEnabled(False)
                     var.ui.btnAdelante.setEnabled(False)
@@ -397,8 +357,11 @@ class Main(QMainWindow):
             print("Error: %s" % str(error))
 
     def abrir_historial(self):
+        # Funcion encargada de abrir una pestaña nueva que contiene el historial de búsqueda
         try:
+            # Crea el objeto del Widget del historial pasándole la ventana del navegador como argumento
             historial = WidgetHistorial(self)
+
             self.setWindowTitle("PyQtBrowser - Historial")
             icon = QtGui.QIcon()
             icon.addPixmap(QtGui.QPixmap("img/history.png"), QtGui.QIcon.Active, QtGui.QIcon.On)
@@ -419,36 +382,49 @@ class Main(QMainWindow):
             print("Error al abrir historial: %s" % str(error))
 
     def cargar_historial(self, historial):
+        # Funcion encargada de pasarle una tarea nueva al hilo trabajador del navegador de cargar el historial
+        # de la base de datos
         try:
-            if self.hilo_historial.receivers(self.hilo_historial.historialRecibido) > 0:
-                self.hilo_historial.historialRecibido.disconnect()
-            self.hilo_historial.historialRecibido.connect(historial.cargar_historial)
-            self.hilo_historial.anadir_tarea(self.hilo_historial.cargar_historial)
+            # Si ya hay una funcion escuchando el evento de historialRecibido, la desconecta
+            if self.hilo_trab.receivers(self.hilo_trab.historialRecibido) > 0:
+                self.hilo_trab.historialRecibido.disconnect()
+
+            # Conecta el evento del hilo trabajador que emite al recibir el historial de la BD, con la funcion de
+            # cargar historial del objeto WidgetHistorial
+            self.hilo_trab.historialRecibido.connect(historial.cargar_historial)
+
+            # Añade una tarea de carga de historial al hilo trabajador
+            self.hilo_trab.anadir_tarea(self.hilo_trab.cargar_historial)
         except Exception as error:
             print("Error al cargar historial: %s" % str(error))
 
     def borrar_entrada_historial(self, idx, historial):
-        self.hilo_historial.anadir_tarea(lambda id=idx: self.hilo_historial.borrar_entrada(idx))
-        self.hilo_historial.anadir_tarea(lambda his=historial: self.cargar_historial(his))
+        # Esta funcion simplemente le pasa 2 tareas al hilo trabajador: borrar una entrada de historial con indice idx
+        # Y volver a cargar el historial en el widget de historial pasado en argumentos
+
+        self.hilo_trab.anadir_tarea(lambda id=idx: self.hilo_trab.borrar_entrada(idx))
+        self.hilo_trab.anadir_tarea(lambda his=historial: self.cargar_historial(his))
 
     def anadir_favorito(self):
         try:
+            # Obtiene el objeto QWebEnginePage del widget actual
             curpage = var.ui.tabWidget.currentWidget().page()
-            self.hilo_historial.anadir_tarea(lambda pag=curpage:
-                                             conexion.anadir_favorito(pag))
+
+            # Pasa la tarea de añadir la pagina actual a favoritos a la base de datos al hilo trabajador
+            self.hilo_trab.anadir_tarea(lambda pag=curpage: conexion.anadir_favorito(pag))
+
+            # Inserta un marcador en la barra de marcadores
             self.insertar_marcador(curpage.title(), curpage.url().toString(), curpage.icon().pixmap(
                 curpage.icon().actualSize(QtCore.QSize(16, 16))))
+
+            # Actualiza el icono del boton de favoritos del navegador
             self.actualizar_icono_fav(True)
         except Exception as error:
             print("Error al anadir favorito: %s " % str(error))
 
-    def cargar_favoritos(self):
-        try:
-            self.hilo_historial.anadir_tarea(self.hilo_historial.cargar_favoritos)
-        except Exception as error:
-            print("Error al cargar favoritos: %s" % str(error))
-
     def mostrar_favoritos(self, query):
+        # Esta funcion recibe un objeto de QSqlQuery del evento favoritosRecibidos emitido por el hilo trabajador
+        # E itera el objeto QSqlQuery para insertar los marcadores en la barra
         try:
             while query.next():
                 pixmap = QtGui.QPixmap()
@@ -461,6 +437,7 @@ class Main(QMainWindow):
             print("Error al mostrar favoritos: %s" % str(error))
 
     def insertar_marcador(self, titulo, url, icono):
+        # Funcion encargada de crear un boton con los parametros indicados e insertarlo en la barra de marcadores
         try:
             boton = QtWidgets.QPushButton(var.ui.widgetMarcadores)
             tam = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
@@ -512,11 +489,10 @@ class Main(QMainWindow):
 
     def comprobar_fav(self, url):
         try:
-            if self.hilo_historial.receivers(self.hilo_historial.paginaFavorita) > 0:
-                self.hilo_historial.paginaFavorita.disconnect()
-            self.hilo_historial.paginaFavorita.connect(self.actualizar_icono_fav)
-            self.hilo_historial.anadir_tarea(lambda url=url:
-                                             self.hilo_historial.comprobar_favorito(url))
+            if self.hilo_trab.receivers(self.hilo_trab.paginaFavorita) > 0:
+                self.hilo_trab.paginaFavorita.disconnect()
+            self.hilo_trab.paginaFavorita.connect(self.actualizar_icono_fav)
+            self.hilo_trab.anadir_tarea(lambda url=url: self.hilo_trab.comprobar_favorito(url))
         except Exception as error:
             print("Error al comprobar favorito : %s" % str(error))
 
@@ -531,7 +507,6 @@ class Main(QMainWindow):
             var.ui.widgetMarcadores.show()
         else:
             var.ui.widgetMarcadores.hide()
-
 
 if __name__ == '__main__':
     app = QApplication([])
